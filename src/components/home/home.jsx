c90d0d23ff522266750d8e6a0de0ab5eac9f33f1
Vathsan <srivaths311@gmail.com>
2020-06-27 21:05:41 +0530

    Gallery

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -3,54 +3,75 @@ import ReactPlayer from 'react-player'
 
 import './home.css'
 
-import Karthik from '../../assets/img/karthik.jpg'
+import kharthik from '../../assets/img/karthik.jpg'
 import still1 from '../../assets/img/still1.jpg'
-import still2 from '../../assets/img/still2.jpg'
+import promo from '../../assets/img/promo.png'
 import still3 from '../../assets/img/still3.jpg'
+
+import Image1 from '../../assets/img/gallery/image1.jpg'
+import Image2 from '../../assets/img/gallery/image2.jpg'
+import Image3 from '../../assets/img/gallery/image3.jpg'
+import Image4 from '../../assets/img/gallery/image4.jpg'
+import Image5 from '../../assets/img/gallery/image5.jpg'
+import Image6 from '../../assets/img/gallery/image6.jpg'
 
 class Home extends Component {
     render() {
         return (
             <>
-                <div class="parallax-container" data-parallax="scroll" data-image-src={Karthik}></div>
+                <div class="parallax-container" data-parallax="scroll" data-image-src={kharthik}></div>
                 <section id="info">
                     <div class="container padding-top">
-                        <h2 class="display-6">Kharthik Palani</h2>
-                        <small>Kharthik Phalani is an Indian cinematographer who works in the Indian film industry. After completing his Under Graduation in Physics, He did Diploma in Film Technology (Cinematography) in FiIm & Television Institute of Tamilnadu, and started assisting Tirru from 2009.</small>
-                        <h2 class="display-6 career">Career</h2>
-                        <small>
-                            <p>
+                        <h6 class="montserrat">Kharthik Phalani is an Indian cinematographer who works in the Indian film industry. After completing his Under Graduation in Physics, He did Diploma in Film Technology (Cinematography) in FiIm & Television Institute of Tamilnadu, and started assisting Tirru from 2009.</h6>
+                        <h2 class="display-6 career montserrat font-var">Career</h2>
+                        <h6 class="montserrat">
+                            <p class="montserrat">
                             He completed Diploma in Film Technology (Cinematography) in FiIm & Television Institute of Tamilnadu and started assisting DOP Mr.Tirru in 2009. Assisting on more than 600 commercials there by collaborating with production houses in India such as Nirvana, Corcoise films, Bang Bang films, sunrise films, Hot films, Anonymous Films, Opticus Inc, soda films, Foot candle films, etc and brands like Center Fresh, Slice, Miranda, KFC Krushers, Tata Indicom, Hutch - Vodafone, Kalyan Jewellers, Prince Jewellers, sangam Suitings, GRT jewellers, Chennai silks, Manapuram Gold, Volini, Canon, Philips, etc.
                             </p>
-                            <p>
+                            <p class="montserrat">
                             Later he went on to became an independent cinematographer and started working on commercials in Mumbai. He also did couple of indie feature films in Hindi and Telugu, and continued to do lots of commercials and promos for Netflix.
                             </p>
-                            <p>
-                            He garnered acclaim for his work in Penguin (2020). A critic from The Times of India noted that "Kharthik Phalani's visuals" help set "set the eerie mood" whilst a critic from The Hindu stated how "The filmmaker, however, revelation in terms of his visual sense. Two shots come to mind that are beautifully imagined and shot".
+                            <p class="montserrat">
+                            He garnered acclaim for his work in Penguin (2020). A critic from The Times of India noted that "Kharthik Phalani's visuals" help "set the eerie mood" whilst a critic from The Hindu stated how "The filmmaker, however, revelation in terms of his visual sense. Two shots come to mind that are beautifully imagined and shot".
                             </p>
-                        </small>
+                        </h6>
                     </div>
                 </section>
                 <div class="parallax-container" data-parallax="scroll" data-image-src={still1}></div>
                 <section id="penguin">
                     <div class="container padding-top">
-                        <h2 class="display-6">Penguin</h2>
-                        <ReactPlayer url="https://vimeo.com/285496498"/>
+                        <h2 class="display-6 montserrat font-var">Penguin (2020)</h2>
+                        <ReactPlayer url="https://www.youtube.com/watch?v=1Mwp1CfFV-k"/>
                     </div>
                 </section>
-                <div class="parallax-container" data-parallax="scroll" data-image-src={still2}></div>
+                <div class="parallax-container" data-parallax="scroll" data-image-src={promo}></div>
                 <section id="penguin">
                     <div class="container padding-top">
-                        <h2 class="display-6">Promo</h2>
+                        <h2 class="display-6 montserrat font-var">Promo</h2>
                         <div class="players">
-                            <ReactPlayer url="https://vimeo.com/269800280"/>
-                            <ReactPlayer url="https://vimeo.com/265532251"/>
+                            <ReactPlayer url="https://vimeo.com/269800280" class="video"/>
+                            <ReactPlayer url="https://vimeo.com/265532251" class="video"/>
                         </div>
-                        
                     </div>
                 </section>
                 <div class="parallax-container" data-parallax="scroll" data-image-src={still3}></div>
-
+                <section id="gallery">
+                    <div class="container padding-top">
+                        <h2 class="display-6 montserrat font-var">Gallery</h2>
+                        <div class="container">
+                            <div class="col">
+                                <div class="gallery">
+                                    <img src={Image1} alt="" class="thumbs"/>
+                                    <img src={Image2} alt="" class="thumbs"/>
+                                    <img src={Image3} alt="" class="thumbs"/>
+                                    <img src={Image4} alt="" class="thumbs"/>
+                                    <img src={Image5} alt="" class="thumbs"/>
+                                    <img src={Image6} alt="" class="thumbs"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
 
             </>
         );
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -9,7 +9,7 @@ class NavBar extends Component {
 
     render() {
         return (
-            <nav class="navbar navbar-dark navbar-expand-lg sticky-top bg-black">
+            <nav class="navbar navbar-dark navbar-expand-lg sticky-top bg-black nav-height">
                 <div class="container-fluid ">
                     <NavLink className="navbar-brand" to="/" id="navbar-brand"><img src={Logo} alt="" id="logo"></img></NavLink>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
@@ -18,7 +18,7 @@ class NavBar extends Component {
                     <div className="collapse navbar-collapse align-class" id="navbarSupportedContent">
                         <ul class="nav navbar-nav ml-auto">
                             <li class="nav-item text-center" id="product-btn">
-                                <NavLink to="/" className="nav-link avenir"><span>Director Of Photography</span></NavLink>
+                                <NavLink to="/" className="nav-link montserrat"><span class="font-var">Director of Photography</span></NavLink>
                             </li>
                         </ul>
                     </div>
