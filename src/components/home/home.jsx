505c51decec2d67806f0286bea42b61ecb5e9d19
Vathsan <srivaths311@gmail.com>
2020-06-28 13:29:23 +0530

    Smaller

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -6,7 +6,7 @@ import './home.css'
 import kharthik from '../../assets/img/karthik.jpg'
 import still1 from '../../assets/img/still1.jpg'
 import promo from '../../assets/img/promo.png'
-import still3 from '../../assets/img/still3.jpg'
+import still3 from '../../assets/img/still2.jpg'
 
 import Image1 from '../../assets/img/gallery/image1.jpg'
 import Image2 from '../../assets/img/gallery/image2.jpg'
@@ -27,16 +27,16 @@ class Home extends Component {
                 <div class="parallax-container" data-parallax="scroll" data-image-src={kharthik}></div>
                 <section id="info">
                     <div class="container padding-top">
-                        <h6 class="montserrat">Kharthik Phalani is an Indian cinematographer who works in the Indian film industry. After completing his Under Graduation in Physics, He did Diploma in Film Technology (Cinematography) in FiIm & Television Institute of Tamilnadu, and started assisting Tirru from 2009.</h6>
-                        <h2 class="display-6 career montserrat font-var">Career</h2>
+                        <h6 class="montserrat content">Kharthik Phalani is an Indian cinematographer who works in the Indian film industry. After completing his Under Graduation in Physics, He did Diploma in Film Technology (Cinematography) in FiIm & Television Institute of Tamilnadu, and started assisting Tirru from 2009.</h6>
+                        <h2 class="display-6 career montserrat font-var content-title">Career</h2>
                         <h6 class="montserrat">
-                            <p class="montserrat">
+                            <p class="montserrat content">
                                 He completed Diploma in Film Technology (Cinematography) in FiIm & Television Institute of Tamilnadu and started assisting DOP Mr.Tirru in 2009. Assisting on more than 600 commercials there by collaborating with production houses in India such as Nirvana, Corcoise films, Bang Bang films, sunrise films, Hot films, Anonymous Films, Opticus Inc, soda films, Foot candle films, etc and brands like Center Fresh, Slice, Miranda, KFC Krushers, Tata Indicom, Hutch - Vodafone, Kalyan Jewellers, Prince Jewellers, sangam Suitings, GRT jewellers, Chennai silks, Manapuram Gold, Volini, Canon, Philips, etc.
                             </p>
-                            <p class="montserrat">
+                            <p class="montserrat content">
                                 Later he went on to became an independent cinematographer and started working on commercials in Mumbai. He also did couple of indie feature films in Hindi and Telugu, and continued to do lots of commercials and promos for Netflix.
                             </p>
-                            <p class="montserrat">
+                            <p class="montserrat content">
                                 He garnered acclaim for his work in Penguin (2020). A critic from The Times of India noted that "Kharthik Phalani's visuals" help "set the eerie mood" whilst a critic from The Hindu stated how "The filmmaker, however, revelation in terms of his visual sense. Two shots come to mind that are beautifully imagined and shot".
                             </p>
                         </h6>
@@ -45,24 +45,25 @@ class Home extends Component {
                 <div class="parallax-container" data-parallax="scroll" data-image-src={still1}></div>
                 <section id="penguin">
                     <div class="container padding-top">
-                        <h2 class="display-6 montserrat font-var">Penguin (2020)</h2>
+                        <h2 class="display-6 montserrat font-var content-title">Penguin (2020)</h2>
                         <ReactPlayer url="https://www.youtube.com/watch?v=1Mwp1CfFV-k" />
                     </div>
                 </section>
                 <div class="parallax-container" data-parallax="scroll" data-image-src={promo}></div>
                 <section id="penguin">
                     <div class="container padding-top">
-                        <h2 class="display-6 montserrat font-var">Promo</h2>
+                        <h2 class="display-6 montserrat font-var content-title">Promo</h2>
                         <div class="players">
                             <ReactPlayer url="https://vimeo.com/269800280" class="video" />
                             <ReactPlayer url="https://vimeo.com/265532251" class="video" />
                         </div>
                     </div>
                 </section>
-                <div class="parallax-container" data-parallax="scroll" data-image-src={still3}></div>
+                <div class="parallax-container" data-parallax="scroll" data-image-src={still3}>
+                    <p class="montserrat font-var gallery-title padding-top-gallery">Gallery</p>
+                </div>
                 <section id="gallery">
                     <div class="container padding-top">
-                        <h2 class="display-6 montserrat font-var">Gallery</h2>
                         <div class="container">
                             <div class="col">
                                 <div class="gallery">
