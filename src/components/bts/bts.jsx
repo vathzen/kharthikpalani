453bca2b847675b03556621e79cba047571c867c
Vathsan <srivaths311@gmail.com>
2020-07-03 12:34:55 +0530

    Navlink Fix

--- src/components/bts/bts.jsx
+++ src/components/bts/bts.jsx
@@ -8,7 +8,7 @@ class BTS extends Component{
             <>
                 <div class="container padding-top">
                     <div class="row">
-                        <h1 class="display-2 montserrat content-title">Behind the Scenes</h1>
+                        <h1 class="display-4 montserrat content-title">Behind the Scenes</h1>
                     </div>
                     <div class="row">
                         ksajshfnjksafjkasfjksfaskfskfklsflksfnsjkfnskjfnsjkflsnfdiaskgnasfljh kj,m f nikjnsfksjf f hs,fsiufkj nsifuskjh nfsukjf sn,ukhf nsfsajkf 
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom'
-
+import { NavLink } from 'react-router-dom';
 import './navbar.css';
 
 import Logo from '../../assets/img/logo.png'
-import { Element,scroller,animateScroll } from 'react-scroll'
+import { scroller,animateScroll } from 'react-scroll'
 
 class NavBar extends Component {
 
@@ -37,14 +36,14 @@ class NavBar extends Component {
                 </button>
                 <div className="collapse navbar-collapse w-100" id="navbarSupportedContent">
                     <ul class="navbar-nav w-100 justify-content-center">
-                        <li class="nav-item ">
+                        <li class="nav-item">
                             <a class="nav-link montserrat" onClick={() => this.scrollToInfo()}>ABOUT</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link montserrat" onClick={() => this.scrollToPenguin()}>WORK</a>
                         </li>
                         <li class="nav-item">
-                            <NavLink to="/bts" class="nav-link montserrat">BTS</NavLink>
+                            <a class="nav-link montserrat" href="/bts">BTS</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link montserrat" onClick={() => this.scrollToContact()}>CONTACT</a>
@@ -52,7 +51,7 @@ class NavBar extends Component {
                     </ul>
                     <ul class="nav navbar-nav ml-auto w-100 justify-content-end">
                         <li class="nav-item text-center" id="product-btn">
-                            <NavLink to="" className="nav-link montserrat"><span class="font-var">Director of Photography</span></NavLink>
+                            <a className="nav-link montserrat active"><span class="font-var">Director of Photography</span></a>
                         </li>
                     </ul>
                 </div>
