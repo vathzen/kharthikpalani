b79a0ed29e39b52459c88a6df7222397461919ff
Vathsan <srivaths311@gmail.com>
2020-07-09 23:53:14 +0530

    Individual Articles

--- src/components/press/press.jsx
+++ src/components/press/press.jsx
@@ -1,36 +1,37 @@
 import React, { Component } from 'react'
-
 import './press.css'
+
+import {Link} from 'react-router-dom'
 
 import CE from '../../assets/img/press/CE.jpeg';
 import Vik from '../../assets/img/press/Vik.jpeg';
 import Din from '../../assets/img/press/Din.jpeg'
 
-class Press extends Component{
-    render(){
-        return(
+class Press extends Component {
+    render() {
+        return (
             <div id="outer">
                 <div class="container-fluid padding-top container-margin">
                     <div class="row montserrat content-title font-white d-flex justify-content-center margin-bottom">PRESS</div>
                     <div class="row">
-                        <div class="col-4 font-white d-flex flex-column align-items-center">
-                            <img src={CE} alt="" width="100%"/>
+                        <div class="col-4 font-white d-flex flex-column align-items-start">
+                            <Link to="/press/cexpress-article"><img src={CE} alt="" width="100%" /></Link>
                             <h2 class="news-title montserrat">Cinema Express</h2>
                             <p class="montserrat content font-small">Khartik talks about how the story lent itself to his style of cinematography in this film. Motherhood, he identifies, is the underlying emotion of the film.</p>
-                            <a href="https://www.cinemaexpress.com/stories/interviews/2020/jun/23/penguin-cinematographer-kharthik-palani-no-recent-tamil-film-has-as-many-wide-shots-19053.html" target="_blank" class="btn btn-outline-light">Read Full Article</a>
+                            <p class="montserrat content font-small">Interview &#183; Penguin</p>
                         </div>
-                        <div class="col-4 font-white d-flex flex-column align-items-center">
-                            <img src={Vik} alt="" width="100%"/>
+                        <div class="col-4 font-white d-flex flex-column align-items-start">
+                            <Link to="/press/vikatan-article"><img src={Vik} alt="" width="100%" /></Link>
                             <h2 class="news-title montserrat">Vikatan</h2>
                             <p class="montserrat content font-small">`பெண்குயின்' படத்தின் ஒளிப்பதிவுக்காக மிகவும் பாராட்டப்பட்ட கார்த்திக் பழனி, தன் அனுபவங்களைப் பகிர்ந்துகொள்கிறார்.</p>
-                            <a href="https://cinema.vikatan.com/tamil-cinema/cinematographer-karthik-palani-shares-his-penguin-movie-experience" target="_blank" class="btn btn-outline-light">Read Full Article</a>
+                            <p class="montserrat content font-small">Interview &#183; Penguin</p>
                         </div>
-                        <div class="col-4 font-white d-flex flex-column align-items-center">
-                            <img src={Din} alt="" width="100%"/>
+                        <div class="col-4 font-white d-flex flex-column align-items-start">
+                        <Link to="/press/dinamani-article"><img src={Din} alt="" width="100%" /></Link>
                             <h2 class="news-title montserrat">Dinamani</h2>
-                            <p class="montserrat content font-small">"பெண்குயின்'... முதல் படத்திலேயே அத்தனை நேர்த்தியான உழைப்பைத் தந்திருக்கிறார் கார்த்திக் பழனி.</p>
-                            <a href="https://www.dinamani.com/weekly-supplements/dinamani-kondattam/2020/jul/05/it-is-difficult-to-make-a-simple-film-3432886.html" target="_blank" class="btn btn-outline-light">Read Full Article</a>
-                        </div>  
+                            <p class="montserrat content font-small">'பெண்குயின்'... முதல் படத்திலேயே அத்தனை நேர்த்தியான உழைப்பைத் தந்திருக்கிறார் கார்த்திக் பழனி.</p>
+                            <p class="montserrat content font-small">Interview &#183; Penguin</p>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/components/routes/routes.jsx
+++ src/components/routes/routes.jsx
@@ -7,6 +7,10 @@ import Press from '../press/press'
 import Social from '../social/social';
 import About from '../about/about';
 
+import Dinamani from '../press/articles/dinamani';
+import Vikatan from '../press/articles/vikatan';
+import CExpress from '../press/articles/cexpress';
+
 export default function Routes(){
     return(
         <Switch>
@@ -15,6 +19,10 @@ export default function Routes(){
             <Route exact path='/press' component = {Press} />
             <Route exact path='/about' component = {About} />
             <Route exact path='/social' component = {Social} />
+
+            <Route exact path='/press/dinamani-article' component = {Dinamani} />
+            <Route exact path='/press/vikatan-article' component = {Vikatan} />
+            <Route exact path='/press/cexpress-article' component = {CExpress} />
         </Switch>
     );
 }
