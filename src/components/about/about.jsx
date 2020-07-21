361a31b9886d52b23c56e1779dfff74e23ba62d5
Vathsan <srivaths311@gmail.com>
2020-07-21 18:22:50 +0530

    Mobile opti

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -9,11 +9,11 @@ class About extends Component{
         return(
             <div id="about-page">
                 <div class="container pad-bottom padding-top">
-                    <div class="row">
-                        <div class="col d-flex flex-column align-items-center">
+                    <div class="row about-row">
+                        <div class="col about-col d-flex flex-column align-items-center">
                             <img src={Still} alt="" width="80%"/>
                         </div>
-                        <div class="col d-flex flex-column justify-content-around">
+                        <div class="col about-col d-flex flex-column justify-content-around mobile-pad">
                             <p class="nhaasgrotesk about-text">Kharthik Phalani is an Indian cinematographer who works in the Indian film industry. After completing his Under Graduation in Physics, He did Diploma in Film Technology (Cinematography) in Film & Television Institute of Tamilnadu, and started assisting Tirru from 2009.</p>
                             <p class="nhaasgrotesk about-text">He completed Diploma in Film Technology (Cinematography) in FiIm & Television Institute of Tamilnadu and started assisting DOP Mr.Tirru in 2009. Assisting on more than 600 commercials there by collaborating with production houses in India such as Nirvana, Corcoise films, Bang Bang films, sunrise films, Hot films, Anonymous Films, Opticus Inc, soda films, Foot candle films, etc and brands like Center Fresh, Slice, Miranda, KFC Krushers, Tata Indicom, Hutch - Vodafone, Kalyan Jewellers, Prince Jewellers, sangam Suitings, GRT jewellers, Chennai silks, Manapuram Gold, Volini, Canon, Philips, etc.</p>
                             <p class="nhaasgrotesk about-text">Later he went on to became an independent cinematographer and started working on commercials in Mumbai. He also did couple of indie feature films in Hindi and Telugu, and continued to do lots of commercials and promos for Netflix.</p>
--- src/components/press/press.jsx
+++ src/components/press/press.jsx
@@ -12,21 +12,21 @@ class Press extends Component {
         return (
             <div id="outer">
                 <div class="container-fluid padding-top container-margin">
-                    <div class="row">
-                        <div class="col-4  d-flex flex-column align-items-start">
+                    <div class="row press-row">
+                        <div class="col press-col d-flex flex-column align-items-start">
                             <Link to="/press/cexpress-article"><img src={CE} alt="" width="100%" /></Link>
                             <h2 class="news-title montserrat">Cinema Express</h2>
                             <p class="montserrat content font-small">Khartik talks about how the story lent itself to his style of cinematography in this film. Motherhood, he identifies, is the underlying emotion of the film.</p>
                             <p class="montserrat content font-small">Interview &#183; Penguin</p>
                         </div>
-                        <div class="col-4  d-flex flex-column align-items-start">
+                        <div class="col press-col d-flex flex-column align-items-start">
                             <Link to="/press/vikatan-article"><img src={Vik} alt="" width="100%" /></Link>
                             <h2 class="news-title montserrat">Vikatan</h2>
                             <p class="montserrat content font-small">`பெண்குயின்' படத்தின் ஒளிப்பதிவுக்காக மிகவும் பாராட்டப்பட்ட கார்த்திக் பழனி, தன் அனுபவங்களைப் பகிர்ந்துகொள்கிறார்.</p>
                             <p class="montserrat content font-small">Interview &#183; Penguin</p>
                         </div>
-                        <div class="col-4  d-flex flex-column align-items-start">
-                        <Link to="/press/dinamani-article"><img src={Din} alt="" width="100%" /></Link>
+                        <div class="col press-col d-flex flex-column align-items-start">
+                            <Link to="/press/dinamani-article"><img src={Din} alt="" width="100%" /></Link>
                             <h2 class="news-title montserrat">Dinamani</h2>
                             <p class="montserrat content font-small">'பெண்குயின்'... முதல் படத்திலேயே அத்தனை நேர்த்தியான உழைப்பைத் தந்திருக்கிறார் கார்த்திக் பழனி.</p>
                             <p class="montserrat content font-small">Interview &#183; Penguin</p>
